30139fae9b796654e7b636bc38c53db790528b90
kubascik <33346194+kubascik@users.noreply.github.com>
2022-08-10 11:57:14 +0200

    Update esxi_upgrade.sh

--- esxi_upgrade.sh
+++ esxi_upgrade.sh
@@ -1,2 +1,2 @@
-esxcli software sources profile list --depot=/vmfs/volumes/datastore1/
+esxcli software sources profile list --depot=depot_location
 esxcli software profile update --depot=depot_location --profile=profile_name --dry-run
