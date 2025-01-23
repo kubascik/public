3cb0c348ed8cc62fc3be8a568083fd1826109f63
kubascik <33346194+kubascik@users.noreply.github.com>
2025-01-23 13:56:48 +0100

    Update orapki.sh
    added JAVA_HOME variable

--- orapki.sh
+++ orapki.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # set classpath for orapki - align this to your local SQLcl installation
+JAVA_HOME=$HOME/apps/jdk
 SQLCL=$(dirname $(which sql))/../lib
 CLASSPATH=${SQLCL}/oraclepki.jar:${SQLCL}/osdt_core.jar:${SQLCL}/osdt_cert.jar
 # simulate orapki command
-java -classpath ${CLASSPATH} oracle.security.pki.textui.OraclePKITextUI "$@"
+$JAVA_HOME/bin/java -classpath ${CLASSPATH} oracle.security.pki.textui.OraclePKITextUI "$@"
