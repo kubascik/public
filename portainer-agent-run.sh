297762ffde941adc806c157a2150b0b6c50c863c
kubascik <33346194+kubascik@users.noreply.github.com>
2025-05-18 19:40:26 +0200

    Create portainer-agent-run.sh

---
+++ portainer-agent-run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 9002:9001 --name portainer_agent2 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
